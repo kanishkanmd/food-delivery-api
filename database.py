32fa1c259cb3701dff1739dcba101756e85d890f
kane111 <kanishkanamdeo@hotmail.com>
2021-11-04 23:51:46 +0530

    Add files via upload

---
+++ database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base,sessionmaker
+
+
+engine=create_engine('postgresql://postgres:admin@localhost:5432/testdb',
+    echo=True
+)
+
+Base=declarative_base()
+
+Session=sessionmaker()
